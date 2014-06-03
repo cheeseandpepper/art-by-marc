a780e0619dae8dbfcf03065d13a8d3b8b6776a49
cheeseandpepper <cheeseandpepper@gmail.com>
2014-06-02 22:17:37 -0400

    mail shit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,15 @@
 Rails.application.configure do
   GA.tracker = Rails.application.secrets.google_analytics_code
   config.action_mailer.default_url_options = { host: Rails.application.secrets.host }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'myname@gmail.com',
+    password:             'mypassword',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
